30c26325d42a8f0fd38d438fcabc4481fac7d264
Charles <charlesmotsisi100@gmail.com>
2022-09-23 14:49:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,SafeAreaView, ScrollView, PermissionsAndroid } from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 export default function App() {
+
+  useEffect(()=>{
+
+  },[])
+
+  const checkPermission = async () =>{
+    const hasPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+    
+    if(hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
+      title: "Image gallery app permissions",
+      message: "Image gallery needs your permission to access your photos",
+      buttonPositive: "Ok",
+    })
+    return status === 'granted'
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          
+        </View>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
 
